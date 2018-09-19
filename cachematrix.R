49fc932672d9c264ad89dcaa10a00b2274451a0b
Vaidehi <vgb.2910@gmail.com>
2018-09-19 21:21:26 +0530

    commiting my cachrmatrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes 2x2 matrix as a parameter(Default is used if none given)
+## Uses getter and setter methods to retrieve and set new matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(c(1,2,3,4), nrow=2, ncol=2)) {
+        inv <- matrix(nrow=2,ncol=2)
+        set <- function(y) {
+                x <<- y
+                inv <<- matrix(nrow=2,ncol=2)
+        }
+        get <- function() x
+        setinv <- function(invm) inv <<- invm
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Checkes in cached data if the matrix already exists or finds and sets inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.na(inv[1][1])) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
 }
+
